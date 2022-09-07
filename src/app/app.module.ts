dcb9333b0208d86869095ddead2dd07ea77e5eb0
Luis Alvarez <l.alvarez301194@gmail.com>
2022-09-07 19:18:56 +0200

    uso de interceptores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
-
+import { TimeInterceptor } from './interceptors/time.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +31,9 @@ import { HighlightDirective } from './directives/highlight.directive';
     FormsModule,
     SwiperModule
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:TimeInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
